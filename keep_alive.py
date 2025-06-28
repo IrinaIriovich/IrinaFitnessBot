1e843c351f4e27d0d9e7bae1b2f57455cf1ec67f
IrinaIriovich <popovairapopova@gmail.com>
2025-06-29 01:56:28 +0300

    Update keep_alive.py
    Вернула keep_alive ( в тч отдельный док обновлён)

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,16 @@
-
 from flask import Flask
 from threading import Thread
+import os  # Не забудь этот импорт!
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Бот работает!"
+    return "Бот работает!"  # Render проверяет этот эндпоинт
 
 def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+    port = int(os.environ.get("PORT", 10000))  # Render передаёт PORT через переменные окружения
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
