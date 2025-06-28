92077834c254cad4c981b5feb8afb8ec6256e923
IrinaIriovich <popovairapopova@gmail.com>
2025-06-29 01:39:43 +0300

    Update main.py
    Keep alive удалён (самое начало и самый конец)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from keep_alive import keep_alive
+
 from telegram.ext import ContextTypes
 from datetime import datetime, timedelta, timezone, time as dt_time
 import random
@@ -389,5 +389,4 @@ def main():
 import asyncio
 
 if __name__ == "__main__":
-    keep_alive()
     main()
