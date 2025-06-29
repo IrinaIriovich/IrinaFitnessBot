c70b09922944f2a81f150a6b173e0c5796e26d23
IrinaIriovich <popovairapopova@gmail.com>
2025-06-30 00:25:20 +0300

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Бот работает!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
